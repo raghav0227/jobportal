64bc83802bbe1ea1adec31a4623f9f0a51957103
raghav0227 <167019115+raghav0227@users.noreply.github.com>
2025-07-22 21:59:45 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # For development
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
